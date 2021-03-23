373fffa181538ea749d2285f5d19ab6e86f54f13
amsalinas03 <73053309+amsalinas03@users.noreply.github.com>
2021-03-22 20:21:38 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,12 @@
 // key = "U5hUKGkBeBR1qSw1yecgBx0flv6hjfMR";
 
 var submitButton = document.getElementById("submit")
-var zipCode = document.getElementById("zipcode").textContent
+var zipCode = $("#zipcode")
 // Foursquare API
 
 $(submitButton).click(function (event) {
     event.preventDefault();
-    locationSearch = $(zipCode);
+    locationSearch = zipCode.text();
     $.ajax({
     dataType: "json",
     url: "https://api.foursquare.com/v2/venues/explore?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20180323&limit=1&ll=40.7243,-74.0018&query=coffee",
@@ -20,7 +20,7 @@ $(submitButton).click(function (event) {
     },
     error: function(jqXHR, textStatus, errorThrown) {
       // Code for handling errors
-    console.log(data)
+    console.log()
     }
     })
 })
