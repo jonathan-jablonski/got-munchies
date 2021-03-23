5a7bb12ff9d3be8f9eb4b73bf36ca803f01a3db5
amsalinas03 <73053309+amsalinas03@users.noreply.github.com>
2021-03-22 19:51:12 -0500

    added search js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,23 @@
 // MapQuest API
-var clientSecret = "77gRNKAyNsP05LND";
-var key = "U5hUKGkBeBR1qSw1yecgBx0flv6hjfMR";
+// clientSecret = "77gRNKAyNsP05LND";
+// key = "U5hUKGkBeBR1qSw1yecgBx0flv6hjfMR";
 
+var submitButton = document.getElementById("submit")
+var zipCode = document.getElementById("zipcode").textContent
+// Foursquare API
+
+$(submitButton).click(function (event) {
+    event.preventDefault();
+    locationSearch = $(zipCode).val().trim();
+    $.ajax({
+    dataType: "json",
+    url: "https://api.foursquare.com/v2/venues/explore?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20180323&limit=1&ll=40.7243,-74.0018&query=coffee",
+    data: {locationSearch},
+    success: function( data ) {
+      // Code for handling API response
+      console.log(data);
+     var theCall = response.data;
+    }
+    //error: function(jqXHR, textStatus, errorThrown) {
+      // Code for handling errors
+    console.log(data)
