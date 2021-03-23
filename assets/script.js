ccb0b21dd999913aea356bf66e9dd6effffb795d
aydanegash <73369360+aydanegash@users.noreply.github.com>
2021-03-22 20:52:32 -0500

    updating

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,26 @@
 // MapQuest API
-var clientSecret = "77gRNKAyNsP05LND";
-var key = "U5hUKGkBeBR1qSw1yecgBx0flv6hjfMR";
+// clientSecret = "77gRNKAyNsP05LND";
+// key = "U5hUKGkBeBR1qSw1yecgBx0flv6hjfMR";
 
-//variables 
+var submitButton = document.getElementById("submit")
+var zipCode = document.getElementById("zipcode").textContent
+// Foursquare API
 
+$(submitButton).click(function (event) {
+    event.preventDefault();
+    locationSearch = $(zipCode);
+    $.ajax({
+    dataType: "json",
+    url: "https://api.foursquare.com/v2/venues/explore?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20180323&limit=1&ll=40.7243,-74.0018&query=coffee",
+    data: {locationSearch},
+    success: function( data ) {
+      // Code for handling API response
+      console.log(data);
+     var theCall = response.data;
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      // Code for handling errors
+    console.log(data)
+    }
+    })
+})
