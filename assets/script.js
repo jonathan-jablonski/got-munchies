09235b9dbe5535ce5892ea3525b302a7656a0e98
jonathan-jablonski <jonathanjablonski94@gmail.com>
2021-03-24 21:32:47 -0500

    Edits made with tutoring

--- assets/script.js
+++ assets/script.js
@@ -7,83 +7,96 @@ var zipCode = $("#zipcode")
 var CLIENT_ID =  "SQM3255VG0KCQPCFOPM5RF1S3S22Q2BMNV3OUZEHT4QIQA2H"
 var CLIENT_SECRET = "1NDCPEPMM5FJQV4HHXKQOLFGHOD54VE2DBSKFG41CUQBHLJ4"
 var radiusSelect = $("select#radius").val()
-
-var foodCategory = "4d4b7105d754a06374d81259"
+var currentLocation = '';
+var mapquestAPI = 'http://www.mapquestapi.com/geocoding/v1/address?key=KEY&location=Washington,DC';
+var foodCategory = [];
 // Restaurant selector
 
 $("#mexican").click(function (event) {
   var categoryType = $(this).attr("value");
-  foodCategory.concat(categoryType);
+  foodCategory.push(categoryType);
 });
 $("#italian").click(function (event) {
   var categoryType = $(this).attr("value");
-  foodCategory.concat(categoryType);
+  foodCategory.push(categoryType);
 });
 $("#ethiopian").click(function (event) {
   var categoryType = $(this).attr("value");
-  foodCategory.concat(categoryType);
+  foodCategory.push(categoryType);
 });
 $("#asian").click(function (event) {
   var categoryType = $(this).attr("value");
-  foodCategory.concat(categoryType);
+  foodCategory.push(categoryType);
 });
 $("#cafe").click(function (event) {
   var categoryType = $(this).attr("value");
-  foodCategory.concat(categoryType);
+  foodCategory.push(categoryType);
 });
 $("#palestinian").click(function (event) {
   var categoryType = $(this).attr("value");
-  foodCategory.concat(categoryType);
+  foodCategory.push(categoryType);
 });
+// $('[type="checkbox"]').on('change', function(event) {
+//   console.log(event);
+//   event.preventDefault();
+//   var val = $(this).val();
+//   foodCategory.push(val);
+// });
 // API CODES (now in values in HTML) mexican 4bf58dd8d48988d1c1941735 ethiopian 4bf58dd8d48988d10a941735 italian 4bf58dd8d48988d110941735 asian 4bf58dd8d48988d142941735 cafe 4bf58dd8d48988d16d941735 palestinian 4bf58dd8d48988d115941735
 // Foursquare API
 // Can't figure out how to search by zipcode just city, also can only search in meters so recommend changing values to match
-$(submitButton).click(function (event) {
-    event.preventDefault();
-    locationSearch = zipCode.val();
-    $.ajax({
-    dataType: "json",
-    url: `https://api.foursquare.com/v2/venues/explore?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&near="${locationSearch}"&v=20210324&limit=5&radius=${radiusSelect}&categoryId=${foodCategory}`,
-    data: {locationSearch};
-  event.preventDefault();
-  locationSearch = zipCode.text();
-  $.ajax({
-    dataType: "json",
-    url: `https://api.foursquare.com/v2/venues/explore?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=20180323&limit=1&ll=40.7243,-74.0018&`,
-    data: { locationSearch },
-    success: function (data) {
-      // Code for handling API response
-      console.log(data);
-      var theCall = data;
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-      // Code for handling errors
-      console.log()
-    }
-  })
-});
+// $(submitButton).click(function (event) {
+//     event.preventDefault();
+//     locationSearch = zipCode.val();
+//     $.ajax({
+//     dataType: "json",
+//     url: `https://api.foursquare.com/v2/venues/explore?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&near="${locationSearch}"&v=20210324&limit=5&radius=${radiusSelect}&categoryId=${foodCategory}`,
+//     data: {locationSearch}
+//     });
+// });
 
 // Function to fetch restaurants based on user-specified criteria
 // use categoryIds
 $(submitButton).click(restaurantsByCategory)
-
+// $('[type="checkbox"]').on('change', function(event) {
+//   console.log(event);
+//   event.preventDefault();
+//   var val = $(this).val();
+//   foodCategory.push(val);
+// });
 function restaurantsByCategory(event){
   event.preventDefault();
-  var location = "40.7243, -74.0018";
-  var radius = 156;
-  var categoryId = "4d4b7105d754a06374d81259";
+  var location = `${currentLocation.latitude}, ${currentLocation.longitude}`;
+  var radius = 8046;
+  if (foodCategory.length === 0) {
+    foodCategory.push('4bf58dd8d48988d1c1941735');
+  }
   $.ajax({
     dataType: "json",
-    url: `https://api.foursquare.com/v2/venues/explore?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=20180323&categoryId=${categoryId}&limit=10&ll=${location}&radius=${radius}`,
+    url: `https://api.foursquare.com/v2/venues/explore?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=20180323&categoryId=${foodCategory.join(',')}&limit=10&ll=${location}&radius=${radius}`,
     success: function( data ) {
       // Code for handling API response
-      console.log(data);
-     var theCall = data;
+    $('.modal-card-body').html();
+    // all restaurants
+     var restaurants = data.response.groups[0].items;
+     // city equivalent
+     var location = data.response.headerLocation;
+     var filterBy = data.response.suggestedFilters.filters;
+     console.log(restaurants, location, filterBy);
      $(".modal").addClass("is-active")
-    },
-    })
-})
 
+    //  if (feelingLucky) {
+    //    restaurants[0]
+    //  }
+     restaurants.forEach((restaurant) => {
+       var currentRestaurant = restaurant.venue;
+       var restaurantAddress = currentRestaurant.location;
+       var restaurantName = currentRestaurant.name;
+      $('.modal-card-body').html(`
+      <div class="restaurant-name">${restaurantName}</div>
+      `);
+     })
+    },
     error: function(jqXHR, textStatus, errorThrown) {
       // Code for handling errors
     console.log(jqXHR, textStatus, errorThrown);
@@ -95,7 +108,16 @@ function restaurantsByCategory(event){
 $(".buttonCancelModal").click(function(){
     $(".modal1").removeClass("is-active");
 });
-
+function getLocation() {
+  if (navigator.geolocation) {
+    currentLocation = navigator.geolocation.getCurrentPosition(showPosition);
+  }
+}
+function showPosition(position) {
+  currentLocation = position.coords
+  console.log(currentLocation);
+}
+getLocation();
 // Function to randomly select a restaurant from an array
 // After we have gotten our user's choices from foursquare, 
 // Feeling lucky submit button
