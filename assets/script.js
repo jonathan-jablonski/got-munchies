bb9aaa72486445bac6940e7f602cb0efb634ee54
amsalinas03 <73053309+amsalinas03@users.noreply.github.com>
2021-03-24 19:34:03 -0500

    script

--- assets/script.js
+++ assets/script.js
@@ -45,37 +45,6 @@ $(submitButton).click(function (event) {
     dataType: "json",
     url: `https://api.foursquare.com/v2/venues/explore?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&near="${locationSearch}"&v=20210324&limit=5&radius=${radiusSelect}&categoryId=${foodCategory}`,
     data: {locationSearch},
-=======
-  event.preventDefault();
-  locationSearch = zipCode.text();
-  $.ajax({
-    dataType: "json",
-    url: `https://api.foursquare.com/v2/venues/explore?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=20180323&limit=1&ll=40.7243,-74.0018&`,
-    data: { locationSearch },
-    success: function (data) {
-      // Code for handling API response
-      console.log(data);
-      var theCall = data;
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-      // Code for handling errors
-      console.log()
-    }
-  })
-});
-
-// Function to fetch restaurants based on user-specified criteria
-// use categoryIds
-$(submitButton).click(restaurantsByCategory)
-
-function restaurantsByCategory(event){
-  event.preventDefault();
-  var location = "40.7243, -74.0018";
-  var radius = 156;
-  var categoryId = "4d4b7105d754a06374d81259";
-  $.ajax({
-    dataType: "json",
-    url: `https://api.foursquare.com/v2/venues/explore?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=20180323&categoryId=${categoryId}&limit=10&ll=${location}&radius=${radius}`,
     success: function( data ) {
       // Code for handling API response
       console.log(data);
@@ -83,26 +52,3 @@ function restaurantsByCategory(event){
     },
     })
 })
-
-    error: function(jqXHR, textStatus, errorThrown) {
-      // Code for handling errors
-    console.log(jqXHR, textStatus, errorThrown);
-    }
-    // Checkbox click to this function
-    // target checkboxes
-  })
-};
-
-
-// Function to randomly select a restaurant from an array
-// After we have gotten our user's choices from foursquare, 
-// Feeling lucky submit button
-// Try Again? Start Over?
-
-
-// Function that fetches map of restaurant's latitude and longitude - run with mapquest and at the time of submission
-
-
-// Function for pop-up modal that displays restaurant, address, map of surrounding area
-
-// Function for pop-up modal that displays random restaurant info
