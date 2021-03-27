aae39f032872ae9cd45f3c837a6909b7ae77b6bf
jonathan-jablonski <jonathanjablonski94@gmail.com>
2021-03-26 22:37:24 -0500

    Updating script.js

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,8 @@
 // clientSecret = "77gRNKAyNsP05LND";
 // key = "U5hUKGkBeBR1qSw1yecgBx0flv6hjfMR";
 
-var randomButton = document.getElementById ("submitRandom1")
-var submitButton = document.getElementById("submit")
-=======
-
 var submitButton = document.getElementsByClassName("submit")
-
-var zipCode = $("#zipcode")
+var cityName = $("#cityName")
 var CLIENT_ID =  "SQM3255VG0KCQPCFOPM5RF1S3S22Q2BMNV3OUZEHT4QIQA2H"
 var CLIENT_SECRET = "1NDCPEPMM5FJQV4HHXKQOLFGHOD54VE2DBSKFG41CUQBHLJ4"
 var radiusSelect = $("select#radius").val()
@@ -16,7 +11,12 @@ var currentLocation = '';
 var mapquestAPI = 'http://www.mapquestapi.com/geocoding/v1/address?key=KEY&location=Washington,DC';
 var foodCategory = [];
 // Restaurant selector
-
+L.mapquest.key = "U5hUKGkBeBR1qSw1yecgBx0flv6hjfMR";
+var map = L.mapquest.map("map", {
+  center: [40.731701, -73.993411],
+  layers: L.mapquest.tileLayer("map"),
+  zoom: 14,
+});
 $("#mexican").click(function (event) {
   var categoryType = $(this).attr("value");
   foodCategory.push(categoryType);
@@ -41,149 +41,26 @@ $("#palestinian").click(function (event) {
   var categoryType = $(this).attr("value");
   foodCategory.push(categoryType);
 });
-// $('[type="checkbox"]').on('change', function(event) {
-//   console.log(event);
-//   event.preventDefault();
-//   var val = $(this).val();
-//   foodCategory.push(val);
-// });
-// API CODES (now in values in HTML) mexican 4bf58dd8d48988d1c1941735 ethiopian 4bf58dd8d48988d10a941735 italian 4bf58dd8d48988d110941735 asian 4bf58dd8d48988d142941735 cafe 4bf58dd8d48988d16d941735 palestinian 4bf58dd8d48988d115941735
-// Foursquare API
-// Can't figure out how to search by zipcode just city, also can only search in meters so recommend changing values to match
-// $(submitButton).click(function (event) {
-//     event.preventDefault();
-//     locationSearch = zipCode.val();
-//     $.ajax({
-//     dataType: "json",
-//     url: `https://api.foursquare.com/v2/venues/explore?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&near="${locationSearch}"&v=20210324&limit=5&radius=${radiusSelect}&categoryId=${foodCategory}`,
-//     data: {locationSearch}
-//     });
-// });
-
 
 // Function to fetch restaurants based on user-specified criteria
 // use categoryIds
-$(submitButton).click(restaurantsByCategory)
-$(randomButton).click(getRandomResturant)
-     
-// $('[type="checkbox"]').on('change', function(event) {
-//   console.log(event);
-//   event.preventDefault();
-//   var val = $(this).val();
-//   foodCategory.push(val);
-// });
-
-
-function restaurantsByCategory(event){
+$(submitButton).click(function(event) {
   event.preventDefault();
-  var location = `${currentLocation.latitude}, ${currentLocation.longitude}`;
-  var radius = 8046;
-  if (foodCategory.length === 0) {
-    foodCategory.push('4bf58dd8d48988d1c1941735');
-  }
-  $.ajax({
-    dataType: "json",
-    url: `https://api.foursquare.com/v2/venues/explore?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=20180323&categoryId=${foodCategory.join(',')}&limit=10&ll=${location}&radius=${radius}`,
-    success: function( data ) {
-      // Code for handling API response
-    $('.modal-card-body').html();
-    // all restaurants
-     var restaurants = data.response.groups[0].items;
-     var randomNumber = Math.floor(Math.random()*restaurants.length)
-    var randomRestaurant = restaurants[randomNumber]
-    console.log(randomRestaurant)
+  var city = cityName.val();
+  console.log(city);
+  L.mapquest.geocoding().geocode([city], function (error, result) {
+    console.log(result);
+    currentLocation = result.results[0].locations[0].latLng;
+    console.log(currentLocation);
+    restaurantsByCategory();
     
-     
-     // city equivalent
-     var location = data.response.headerLocation;
-     var filterBy = data.response.suggestedFilters.filters;
-     console.log(restaurants, location, filterBy);
-     $(".modal").addClass("is-active")
-
-    //  if (feelingLucky) {
-    //    restaurants[0]
-    //  }
-
-     restaurants.forEach((restaurant) => {
-       var currentRestaurant = restaurant.venue;
-       var restaurantAddress = currentRestaurant.location;
-       var restaurantName = currentRestaurant.name;
-      $('.modal-card-body').html(`
-      <div class="restaurant-name">${restaurantName}</div>
-      `);
-     })
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-      // Code for handling errors
-    console.log(jqXHR, textStatus, errorThrown);
-    }
-    // Checkbox click to this function
-    // target checkboxes
-  })
-};
-$(".buttonCancelModal").click(function(){
-    $(".modal1").removeClass("is-active");
+  });
 });
-function getLocation() {
-  if (navigator.geolocation) {
-    currentLocation = navigator.geolocation.getCurrentPosition(showPosition);
-  }
-}
-function showPosition(position) {
-  currentLocation = position.coords
+
+function restaurantsByCategory() {
+  var location = `${currentLocation.lat}, ${currentLocation.lng}`;
   console.log(currentLocation);
-}
-getLocation();
-// Function to randomly select a restaurant from an array
-// After we have gotten our user's choices from foursquare, 
-// Feeling lucky submit button
-// Try Again? Start Over?
-
-
-// Function that fetches map of restaurant's latitude and longitude - run with mapquest and at the time of submission
-
-
-// Function for pop-up modal that displays restaurant, address, map of surrounding area
-
-// Function for pop-up modal that displays random restaurant info
-
-//
-function getRandomRestaurant(event){
-  event.preventDefault();
-  var location = "33.06763348808326, -96.68643319934618";
-=======
-
-// Function to fetch restaurants based on user-specified criteria
-// use categoryIds
-$(submitButton).click(restaurantsByCategory)
-// $('[type="checkbox"]').on('change', function(event) {
-//   console.log(event);
-//   event.preventDefault();
-//   var val = $(this).val();
-//   foodCategory.push(val);
-// });
-// $(submitButton).click(function (event) {
-//   event.preventDefault();
-//   L.mapquest.key = "U5hUKGkBeBR1qSw1yecgBx0flv6hjfMR";
-//   var map = L.mapquest.map("map", {
-//     center: [0, 0],
-//     layers: L.mapquest.tileLayer("map"),
-//     zoom: 14,
-//   });
-//   console.log(map)
-  // var city = $("#zipcode").val();
-  // L.mapquest.geocoding().geocode(city, function (error, result) {
-  //   console.log(result);
-  //   currentLocation = result.results[0].locations[0].latLng;
-  //   console.log(currentLocation);
-  //   restaurantsByCategory();
-    
-  // });
-// });
-function restaurantsByCategory(event){
-  event.preventDefault();
-  var location = `${currentLocation.latitude}, ${currentLocation.longitude}`;
-
+  map.setView(currentLocation)
   var radius = 8046;
   if (foodCategory.length === 0) {
     foodCategory.push('4bf58dd8d48988d1c1941735');
@@ -196,22 +73,6 @@ function restaurantsByCategory(event){
     $('.modal-card-body').html();
     // all restaurants
      var restaurants = data.response.groups[0].items;
-
-     var randomNumber = Math.floor(Math.random()*restaurants.length)
-    var randomRestaurant = restaurants[randomNumber]
-    console.log(randomRestaurant)
-    $('.modal-card-body').html(`
-        <div class="restaurant-name">${randomRestaurant[0].venue.name}</div>
-       `);
-     // city equivalent
-     var location = data.response.headerLocation;
-     var filterBy = data.response.suggestedFilters.filters;
-     console.log(restaurants, location, filterBy);
-     $(".modal").addClass("is-active")
-    //  if (feelingLucky) {
-    //    restaurants[0]
-    //  }
-
      // city equivalent
      var locationFromData = data.response.headerLocation;
      var filterBy = data.response.suggestedFilters.filters;
@@ -221,12 +82,6 @@ function restaurantsByCategory(event){
     //  if (feelingLucky) {
     //    restaurants[0]
     //  }
-    L.mapquest.key = "U5hUKGkBeBR1qSw1yecgBx0flv6hjfMR";
-      var map = L.mapquest.map("map", {
-        center: [restaurants[0].venue.location.lat, restaurants[0].venue.location.lng],
-        layers: L.mapquest.tileLayer("map"),
-        zoom: 14,
-      });
       L.mapquest.geocoding().geocode(restaurants[0].venue.location.formattedAddress.join(','));
       $('.modal-card-body').append(`
         <div class="restaurant-name">${restaurants[0].venue.name}</div>
@@ -240,7 +95,6 @@ function restaurantsByCategory(event){
     //   <div class="restaurant-name">${restaurantName}</div>
     //   `);
     //  })
-
     },
     error: function(jqXHR, textStatus, errorThrown) {
       // Code for handling errors
@@ -249,23 +103,10 @@ function restaurantsByCategory(event){
     // Checkbox click to this function
     // target checkboxes
   })
-
-};
-
 };
 $(".buttonCancelModal").click(function(){
     $(".modal1").removeClass("is-active");
 });
-function getLocation() {
-  if (navigator.geolocation) {
-    currentLocation = navigator.geolocation.getCurrentPosition(showPosition);
-  }
-}
-function showPosition(position) {
-  currentLocation = position.coords
-  console.log(currentLocation);
-}
-getLocation();
 // Function to randomly select a restaurant from an array
 // After we have gotten our user's choices from foursquare, 
 // Feeling lucky submit button
@@ -278,4 +119,3 @@ getLocation();
 // Function for pop-up modal that displays restaurant, address, map of surrounding area
 
 // Function for pop-up modal that displays random restaurant info
-
